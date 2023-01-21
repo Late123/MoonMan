f9a3ae3136410b6dfeb3b6060df63d0657edb5fd
Late123 <69660088@qq.com>
2023-01-21 17:25:59 -0500

    First Push

---
+++ Assets/Scripts/MoonToggle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonToggle : MonoBehaviour
+{
+    //public Rigidbody moon;
+    public float speed = 0.2f;
+    public GameObject close, far;
+    public float t;
+
+    // Start is called before the first frame update
+    private void Start()
+    {   
+        t = 0;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {   
+        if (Input.GetMouseButton(0))
+        {
+            t += speed * Time.deltaTime;
+        }
+        if (Input.GetMouseButton(1)) 
+        {
+            t -= speed * Time.deltaTime;
+        }
+
+        t = Mathf.Clamp(t, 0, 1);
+        transform.position = Vector3.Lerp(far.transform.position, close.transform.position, t);
+        transform.localScale = Vector3.Lerp(far.transform.localScale, close.transform.localScale, t);   
+    }
+}
---
+++ Assets/Scripts/water_script.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+using Cinemachine.Examples;
+public class water_script : MonoBehaviour
+{
+	public GameObject moon;
+    public float far_height = 0.0f;
+    public float close_height = 10.0f;
+    public float move_speed = 100.0f;
+	public float moon_height_when_close = 50.0f;
+	public float moon_height_when_far = 100.0f;
+    private AudioSource audioSource;
+    public AudioClip deathSound;
+    private CharacterMovement characterMovement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterMovement>();
+		float desired_height = get_desired_height();
+		Vector3 new_position = new Vector3(transform.position.x, desired_height, transform.position.z);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        float desired_height = get_desired_height();
+        float new_height = Mathf.MoveTowards(transform.position.y, desired_height, move_speed * Time.fixedDeltaTime);
+
+        Vector3 new_position = new Vector3(transform.position.x, new_height, transform.position.z);
+        transform.position = new_position;
+
+    }
+
+	float get_desired_height() {
+		float moon_height = moon.transform.position.y;
+		float moon_height_range = moon_height_when_far - moon_height_when_close;
+
+		float t = (moon_height - moon_height_when_close) / moon_height_range;
+
+		return Mathf.Lerp(close_height, far_height, t);
+	}
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            audioSource.Stop();
+            audioSource.clip = deathSound;
+            audioSource.Play();
+            characterMovement.Respawn(1.0f);
+        }
+    }
+
+}
